fe4067bbef41603c80613994e06cede90dabe2bb
Sergio Garrido <sergarridom8@gmail.com>
2022-11-04 14:09:01 +0100

    Portfolio añadido

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin: 10,
+    // autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
 
+        500: {
+            items: 2
+        },
+
+        1000: {
+            items: 3
+        },
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[3000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
